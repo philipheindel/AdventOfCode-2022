b57c9fcf8972122209fb63426700266f89915b1f
Philip Heindel <p.e.heindel@gmail.com>
2022-12-08 14:24:45 -0500

    Create solve.ps1

---
+++ Day1/solve.ps1
@@ -0,0 +1,25 @@
+$input = Get-Content .\input.txt
+
+[int]$groupSum = 0
+[int]$totalSum
+[int]$currentMax = 0
+
+ForEach ($line in $input)
+{
+    If ($line -eq "")
+    {
+        If ($groupSum -gt $currentMax)
+        {
+            $currentMax = $groupSum
+        }
+        $groupSum = 0
+    }
+    Else
+    {
+        $groupSum += ([int]$line)
+        $totalSum += ([int]$line)
+    }
+}
+
+Write-Output "Absolute total: $($totalSum)"
+Write-Output "Largest single calory carry: $($currentMax)"
